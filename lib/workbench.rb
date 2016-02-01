23bcc053d1087102026664116eaf0c75bd9c663a
Chris Pomfret <chris.pomfret@bt.com>
2016-02-01 16:16:13 +0000

    target ruby version in .rubocop.yml add workbench test area

---
+++ lib/workbench.rb
@@ -0,0 +1,5 @@
+class Workbench
+  def just_a_placeholder?
+    true
+  end
+end
---
+++ spec/lib/workbench_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Workbench do
+  it 'is just a placeholder' do
+    expect(subject).to be_just_a_placeholder
+  end
+end
